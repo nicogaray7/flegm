d32d769a2bcab38e3f6df908951fe9aa164ef672
nicogaray7 <67230494+nicogaray7@users.noreply.github.com>
2025-01-23 00:51:52 -0500

    f

---
+++ frontend/expo.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Flegm',
+  slug: 'flegm',
+  version: '1.0.0',
+  orientation: 'portrait',
+  web: {
+    bundler: 'webpack',
+    output: 'static',
+  },
+  extra: {
+    apiUrl: process.env.EXPO_PUBLIC_API_URL,
+    cloudinaryCloudName: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    cloudinaryApiKey: process.env.EXPO_PUBLIC_CLOUDINARY_API_KEY,
+  },
+}; 

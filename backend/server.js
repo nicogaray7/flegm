5b5c6a38a870170523fd143eb2198833f8c98c4e
nicogaray7 <67230494+nicogaray7@users.noreply.github.com>
2025-01-24 14:36:24 -0500

    feat: réactivation des déploiements automatiques sur GitHub Pages

--- backend/server.js
+++ backend/server.js
@@ -137,7 +137,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });

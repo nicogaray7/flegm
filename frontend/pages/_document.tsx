7e39758b99e4920df1db98efe221754b5bb10301
nicogaray7 <67230494+nicogaray7@users.noreply.github.com>
2025-02-02 00:12:52 -0500

    feat: ajout de la balise Google AdSense

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,12 +1,25 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
-
     return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          <meta name="google-adsense-account" content="ca-pub-1641130131347247" />
+        </Head>
+        <body className="antialiased text-gray-900 bg-gray-50">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 

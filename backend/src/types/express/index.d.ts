91435351a894a7ef71ad8788fd33a81d2b980ca3
nicogaray7 <67230494+nicogaray7@users.noreply.github.com>
2025-02-05 02:53:24 -0500

    fix: configuration du build pour Render et mise à jour des dépendances

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,7 +1,19 @@
 import { IUser } from '../../models/User';
+import * as express from 'express';
 
 declare global {
   namespace Express {
     interface User extends IUser {}
+    interface Request {
+      user?: IUser;
+    }
   }
-} 
+}
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: IUser;
+  }
+}
+
+export {}; 
